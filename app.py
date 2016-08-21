31721ec39871908e2f4a928adcb1769c1b7eae74
James Cool <jeffreysbrother@gmail.com>
2016-08-20 21:00:41 -0700

    add the vector module

---
+++ app.py
@@ -0,0 +1,13 @@
+from vector import Vector
+
+my_vector = Vector([1, 2, 3])
+print my_vector
+
+my_vector2 = Vector([1, 2, 3])
+my_vector3 = Vector([-1, 2, 3])
+
+#this will return True
+print my_vector == my_vector2
+
+#this will return False
+print my_vector == my_vector3
